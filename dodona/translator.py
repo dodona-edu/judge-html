bfa0d6b3d00b6d465e9d8f24af9df1d01f8dc48f
Quinten Vervynck <vervynck.quinten@gmail.com>
2021-09-08 14:33:09 +0200

    translator opgekuist

--- dodona/translator.py
+++ dodona/translator.py
@@ -77,7 +77,7 @@ class Translator:
     error_translations = {
         Language.EN: {
             ErrorType.INTERNAL_ERROR: "Internal error",
-            ErrorType.COMPILATION_ERROR: "The query is not valid",
+            ErrorType.COMPILATION_ERROR: "The code is not valid",
             ErrorType.MEMORY_LIMIT_EXCEEDED: "Memory limit exceeded",
             ErrorType.TIME_LIMIT_EXCEEDED: "Time limit exceeded",
             ErrorType.OUTPUT_LIMIT_EXCEEDED: "Output limit exceeded",
@@ -89,7 +89,7 @@ class Translator:
         },
         Language.NL: {
             ErrorType.INTERNAL_ERROR: "Interne fout",
-            ErrorType.COMPILATION_ERROR: "Ongeldige query",
+            ErrorType.COMPILATION_ERROR: "Ongeldige code",
             ErrorType.MEMORY_LIMIT_EXCEEDED: "Geheugenlimiet overschreden",
             ErrorType.TIME_LIMIT_EXCEEDED: "Tijdslimiet overschreden",
             ErrorType.OUTPUT_LIMIT_EXCEEDED: "Outputlimiet overschreden",
@@ -103,37 +103,9 @@ class Translator:
 
     text_translations = {
         Language.EN: {
-            Text.ADD_A_SEMICOLON: "Add a semicolon ';' at the end of each SQL query.",
-            Text.SUBMISSION_CONTAINS_MORE_QUERIES: "Error: "
-            "the submitted solution contains more queries ({submitted}) than expected ({expected}). "
-            "Make sure that all queries correctly terminate with a semicolon.",
-            Text.SUBMISSION_CONTAINS_LESS_QUERIES: "Error: "
-            "the submitted solution contains less queries ({submitted}) than expected ({expected}). "
-            "Make sure that all queries correctly terminate with a semicolon.",
-            Text.DIFFERENT_ROW_COUNT: "Expected row count {expected}, your row count was {submitted}.",
-            Text.DIFFERENT_COLUMN_COUNT: "Expected column count {expected}, your column count was {submitted}.",
-            Text.COMPARING_QUERY_OUTPUT_CSV_CONTENT: "Comparing query output csv content",
-            Text.COMPARING_QUERY_OUTPUT_TYPES: "Comparing query output SQL types",
-            Text.QUERY_SHOULD_ORDER_ROWS: "Query should return ordered rows.",
-            Text.QUERY_SHOULD_NOT_ORDER_ROWS: "No explicit row ordering should be enforced in query.",
-            Text.ROWS_ARE_BEING_ORDERED: "rows are being ordered",
-            Text.ROWS_ARE_NOT_BEING_ORDERED: "rows are not being ordered",
+            Text.UNCLOSED_HTML_TAG: "There is a tag that was opened but not closed"
         },
         Language.NL: {
-            Text.ADD_A_SEMICOLON: "Voeg een puntkomma ';' toe aan het einde van elke SQL query.",
-            Text.SUBMISSION_CONTAINS_MORE_QUERIES: "Error: "
-            "de ingediende oplossing bestaat uit meer query's ({submitted}) dan verwacht ({expected}). "
-            "Zorg ervoor dat elke query correct eindigt op een puntkomma.",
-            Text.SUBMISSION_CONTAINS_LESS_QUERIES: "Error: "
-            "de ingediende oplossing bestaat uit minder query's ({submitted}) dan verwacht ({expected}). "
-            "Zorg ervoor dat elke query correct eindigt op een puntkomma.",
-            Text.DIFFERENT_ROW_COUNT: "Verwachtte {expected} rijen, uw aantal rijen is {submitted}.",
-            Text.DIFFERENT_COLUMN_COUNT: "Verwachtte {expected} kolommen, uw aantal kolommen is {submitted}.",
-            Text.COMPARING_QUERY_OUTPUT_CSV_CONTENT: "Vergelijken van de query output in csv formaat",
-            Text.COMPARING_QUERY_OUTPUT_TYPES: "Vergelijken van de query output SQL types",
-            Text.QUERY_SHOULD_ORDER_ROWS: "De query moet de rijen gesorteerd teruggeven.",
-            Text.QUERY_SHOULD_NOT_ORDER_ROWS: "De query mag de rijen niet expliciet gaan sorteren.",
-            Text.ROWS_ARE_BEING_ORDERED: "rijen worden gesorteerd",
-            Text.ROWS_ARE_NOT_BEING_ORDERED: "rijen worden niet gesorteerd",
+            Text.UNCLOSED_HTML_TAG: "Er is een HTML-tag geopend die niet afgesloten werd"
         },
     }
