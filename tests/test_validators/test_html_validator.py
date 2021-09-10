2c3ed8a71f13cdea0f954813c5bb95eab583c7b0
Quinten Vervynck <vervynck.quinten@gmail.com>
2021-09-10 15:40:54 +0200

    wrote tests

--- tests/test_validators/test_html_validator.py
+++ tests/test_validators/test_html_validator.py
@@ -11,7 +11,8 @@ class TestHtmlValidator(unittest.TestCase):
     def test_missing_closing_tag(self):
         # correct tag closing test
         self.validator.validate_content("<div></div>")
-        # incorrect tag closing test
+        self.validator.validate_content("<body><div></div></body>")
+        # incorrect tag closing tests
         with self.assertRaises(MissingClosingTagError):
             self.validator.validate_content("<body><div></div>")
         with self.assertRaises(MissingClosingTagError):
@@ -24,3 +25,20 @@ class TestHtmlValidator(unittest.TestCase):
         with self.assertRaises(InvalidTagError):
             self.validator.validate_content("<jibberjabber></jibberjabber>")
 
+    def test_invalid_attribute(self):
+        # correct attribute test
+        self.validator.validate_content("<html lang='en'></html>")
+        # there is no incorrect attribute checking
+
+    def test_missing_required_attribute(self):
+        pass
+        # no required arguments are set in the json yet
+
+    def test_missing_recommended_attribute(self):
+        # correct required attribute test
+        self.validator.validate_content("<html lang='en'></html>")
+        # incorrect (missing) required attribute test
+        with self.assertRaises(Warnings):  # throws a MissingRecommendedAttributeError but it is collected as Warnings
+            self.validator.validate_content("<html></html>")
+        with self.assertRaises(Warnings):
+            self.validator.validate_content("<html><html><html></html></html></html>")
