90f0988b206f06391527963e28c23777308d3c21
Stijn De Clercq <declercq.stijn@outlook.com>
2021-09-10 11:23:33 +0200

    Build evaluator code

--- html_judge.py
+++ html_judge.py
@@ -1,10 +1,38 @@
 import sys
-from os import path, getcwd, listdir
-from dodona.dodona_command import Judgement, Test, TestCase, Message, ErrorType, Tab, Context
+from os import path
+from dodona.dodona_command import Judgement, Test, TestCase, Message, ErrorType, Tab, Context, DodonaException, \
+    MessagePermission, MessageFormat
 from dodona.dodona_config import DodonaConfig
 from dodona.translator import Translator
-from importlib import import_module
-from inspect import getmembers, isfunction
+from types import ModuleType
+
+
+def build_evaluator_module(config: DodonaConfig) -> ModuleType:
+    """Compile the evaluation code & create a new module"""
+    # Create filepath
+    custom_evaluator_path = path.join(config.resources, "./evaluator.py")
+
+    # Evaluator doesn't exist, throw an exception
+    if not path.exists(custom_evaluator_path):
+        raise DodonaException(
+            config.translator.error_status(ErrorType.INTERNAL_ERROR),
+            permission=MessagePermission.STAFF,
+            description="Could not find evaluator.py script. ",
+            format=MessageFormat.TEXT,
+        )
+
+    # Read raw content of .py file
+    with open(custom_evaluator_path, "r") as fp:
+        # Compile the code into bytecode
+        evaluator_script = compile(fp.read(), "<string>", "exec")
+
+    # Create a new module
+    evaluator_module = ModuleType("evaluation")
+
+    # Build the bytecode & add to the new module
+    exec(evaluator_script, evaluator_module.__dict__)
+
+    return evaluator_module
 
 
 def main():
@@ -21,12 +49,8 @@ def main():
         # Initiate translator
         config.translator = Translator.from_str(config.natural_language)
 
-        custom_evaluator_path = path.join(config.resources, "evaluator.py")
-        print(listdir(getcwd()))
-        # If a custom evaluator exists, try to import and use it
-        if path.exists(custom_evaluator_path):
-            m = import_module(custom_evaluator_path)
-            print(getmembers(m, isfunction))
+        # Compile evaluator code
+        evaluator = build_evaluator_module(config)
 
         with Tab("Tab 1"):
             with Context(), TestCase("Setup test description"):
