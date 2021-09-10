f5da769262d93655659d4225e6c63a964a219d1a
Stijn De Clercq <declercq.stijn@outlook.com>
2021-09-10 12:18:50 +0200

    Remove obsolete test

--- tests/test_validators/test_checks.py
+++ tests/test_validators/test_checks.py
@@ -12,30 +12,3 @@ class TestChecks(unittest.TestCase):
         html_content = load_html_file("nested_attributes")
 
         suite = checks.TestSuite(html_content)
-
-        # Get elements
-        body = suite.element("body")
-        div1 = body.get_child("div")
-        div2 = div1.get_child("div")
-        a = div2.get_child("a")
-
-        # Create checks
-        message = "Test message"
-        grouped_list = [
-            body.exists(),
-            div1.exists(),
-            div2.exists(),
-            a.exists(),
-            a.has_content(message)
-        ]
-
-        # Assign checklist
-        suite.checklist = [
-            checks.all_of(message, grouped_list).display()
-        ]
-
-        # Check that test failed, but the requested message was shown
-        evaluation = suite.evaluate()
-        self.assertEqual(len(evaluation), 1)
-        self.assertFalse(evaluation[0][0])
-        self.assertEqual(evaluation[0][1], message)
