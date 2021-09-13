e0a3fbbaba6f86324750a57ead3b5767889ed304
Quinten Vervynck <vervynck.quinten@gmail.com>
2021-09-13 14:05:36 +0200

    double char checking with positions (lines and pos) feedback & exceptions)

--- validators/double_chars_validator.py
+++ validators/double_chars_validator.py
@@ -1,4 +1,11 @@
+from exceptions.double_char_exceptions import *
+
 class DoubleCharsValidator:
+    def __init__(self):
+        self.opening = ["(", "<", "{", "[", "'", '"']
+        self.closing = [")", ">", "}", "]", "'", '"']
+        self.convert = dict((key, val) for key, val in [*zip(self.opening, self.closing), *zip(self.closing, self.opening)])
+
     def validate_content(self, text: str):
         stack = []
         pos_stack = []
@@ -14,6 +21,7 @@ class DoubleCharsValidator:
         text = list(text)
         i = 0
         end = len(text)
+        line, pos = [1, 1]
         # loop text
         while i < end:
             char = text[i]
@@ -24,8 +32,25 @@ class DoubleCharsValidator:
             # not closing
             elif char in convert:
                 stack.append(char)
-                pos_stack.append(i + 1)  # humans count from 1 not from 0
+                pos_stack.append((line, pos))  # humans count from 1 not from 0
             i += 1
-        print(len(stack))
-        print(stack)
-        print(pos_stack)
+            # update position
+            if char == "\n":
+                line += 1
+                pos = 1
+            else:
+                pos += 1
+
+        # the stack should be empty, if not print remaining things
+        if stack:
+            errors = MultipleMissingCharsError()
+            while stack:
+                char = stack.pop()
+                pos = pos_stack.pop()
+                if char in self.opening:
+                    errors.add(MissingClosingCharError(char, pos))
+                else:
+                    errors.add(MissingOpeningCharError(char, pos))
+            raise errors
+
+
