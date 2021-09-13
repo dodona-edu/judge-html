cdbc363d64977227d4bdef3a3abbaf856a214381
Stijn De Clercq <declercq.stijn@outlook.com>
2021-09-13 16:41:22 +0200

    Add evaluator example file

---
+++ examples/evaluator.py
@@ -0,0 +1,10 @@
+from typing import List
+
+from validators import checks
+
+
+def create_suites(content: str) -> List[checks.TestSuite]:
+    html_suite = checks.TestSuite("HTML", content)
+    css_suite = checks.TestSuite("CSS", content)
+
+    return [html_suite, css_suite]
