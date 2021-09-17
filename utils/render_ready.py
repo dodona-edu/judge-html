06a401e7569ea364466beb9e7b24a6af424688a3
Quinten Vervynck <vervynck.quinten@gmail.com>
2021-09-17 13:09:58 +0200

    rebase main

--- utils/render_ready.py
+++ utils/render_ready.py
@@ -25,7 +25,7 @@ def prep_render(html_content: str) -> str:
 
         new_style = ""
         for r in rs.rules:
-            new_style += f"{r.selector_str}" + "{" + f"{r.name}:{tinycss2.serialize(r.value)}{'!important' if r.important else ''}" + ";}\n   "
+            new_style += f"{r.selector_str}" + "{" + f"{r.name}:{r.value_str}{'!important' if r.important else ''}" + ";}\n   "
 
         style.string = new_style
 
