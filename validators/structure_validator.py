921611702fc6b6864a3fc0cfc7ab3de3b48d6565
Quinten Vervynck <68075051+QuintenVervynck@users.noreply.github.com>
2021-09-21 11:18:44 +0200

    Update validators/structure_validator.py
    Co-authored-by: Stijn De Clercq <60451863+stijndcl@users.noreply.github.com>

--- validators/structure_validator.py
+++ validators/structure_validator.py
@@ -21,7 +21,7 @@ def compare(solution: str, submission: str, trans: Translator, **kwargs):
     possible kwargs:
     * attributes: (default: False) check whether attributes are exactly the same in solution and submission
     * minimal_attributes: (default: False) check whether at least the attributes in solution are supplied in the submisstion
-    * contents: (default: False) check whether the contents of each tag in the solution are exactly the same as in the submisstion
+    * contents: (default: False) check whether the contents of each tag in the solution are exactly the same as in the submission
     Raises a NotTheSame exception if the solution and the submission are not alike
     """
     # structure is always checked
