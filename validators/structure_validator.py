f8116c23ab10e2c9d8f9d054c4d9ed812084e835
Quinten Vervynck <vervynck.quinten@gmail.com>
2021-09-21 15:11:42 +0200

    allow dummy

--- validators/structure_validator.py
+++ validators/structure_validator.py
@@ -35,6 +35,27 @@ def compare(solution: str, submission: str, trans: Translator, **kwargs):
     submission: HtmlElement = fromstring(submission)
     # start checking structure
 
+    def attrs_a_contains_attrs_b(attrs_a, attrs_b, exact_match):
+        # split dummy values from attrs_a
+        dummies = set()
+        exact = {}
+        for a in attrs_a:
+            if node_sol.attrib.get(a).strip() == "DUMMY":
+                dummies.add(a)
+            else:
+                exact.update({a: node_sol.attrib.get(a).strip()})
+        # check if all attrs in a are in b (if exact, all attrs from b must also be in a)
+        for b in attrs_b:
+            if b in exact and exact[b] == node_sub.attrib[b]:
+                exact.pop(b)
+            elif b in dummies:
+                dummies.remove(b)
+            elif exact_match:
+                return False
+        if dummies or exact:
+            return False
+        return True
+
     queue = ([(solution, submission)])
     while queue:
         node_sol, node_sub = queue.pop()
@@ -43,14 +64,14 @@ def compare(solution: str, submission: str, trans: Translator, **kwargs):
             raise NotTheSame(trans.translate(Translator.Text.TAGS_DIFFER), node_sub.sourceline, trans)
         # check attributes if wanted
         if check_attributes:
-            if node_sol.attrib != node_sub.attrib:
+            if not attrs_a_contains_attrs_b(node_sol.attrib, node_sub.attrib, True):
                 raise NotTheSame(trans.translate(Translator.Text.ATTRIBUTES_DIFFER), node_sub.sourceline, trans)
         if check_minimal_attributes:
-            if not set(node_sol.attrib).issubset(set(node_sub.attrib)):
+            if not attrs_a_contains_attrs_b(node_sol.attrib, node_sub.attrib, False):
                 raise NotTheSame(trans.translate(Translator.Text.NOT_ALL_ATTRIBUTES_PRESENT), node_sub.sourceline, trans)
         # check content if wanted
         if check_contents:
-            if node_sol.text != node_sub.text:
+            if node_sol.text.strip() != "DUMMY" and node_sol.text.strip() != node_sub.text.strip():
                 raise NotTheSame(trans.translate(Translator.Text.CONTENTS_DIFFER), node_sub.sourceline, trans)
         # check children of the node
         if len(node_sol.getchildren()) != len(node_sub.getchildren()):
