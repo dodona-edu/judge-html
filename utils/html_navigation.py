f526e7cf7d961d7a967d2dcf20a1ae2714f33b64
Stijn De Clercq <declercq.stijn@outlook.com>
2021-09-22 17:07:48 +0200

    Find elements using emmet syntax

--- utils/html_navigation.py
+++ utils/html_navigation.py
@@ -1,5 +1,5 @@
 import re
-from typing import Optional, Union
+from typing import Optional, Union, List
 from bs4 import BeautifulSoup
 from bs4.element import Tag
 
@@ -17,6 +17,13 @@ def find_child(element: Optional[Union[BeautifulSoup, Tag]],
     if element is None:
         return None
 
+    # Doesn't match only text, so emmet syntax was used
+    if tag and re.match(r"^[a-zA-Z]+$", tag) is None:
+        return _find_emmet(element, tag, from_root)
+
+    # Tags should be lowercase
+    tag = tag.lower()
+
     # No index specified, first child requested
     if index == 0:
         return element.find(tag, recursive=not from_root, **kwargs)
@@ -33,6 +40,73 @@ def find_child(element: Optional[Union[BeautifulSoup, Tag]],
             index = 0
 
         return all_children[index]
+
+
+def _find_emmet(element: Optional[Union[BeautifulSoup, Tag]], path: str, from_root: bool = False) -> Optional[Tag]:
+    """Find an element using emmet syntax"""
+    if element is None:
+        return None
+
+    # Tag must always be in the beginning, otherwise we can't parse it out
+    tag_regex = re.compile(r"^[a-zA-Z]+")
+    id_regex = re.compile(r"#([a-zA-Z0-9_-]+)")
+    index_regex = re.compile(r"\[([0-9]+)\]$")
+
+    # Cannot start with a digit, two hyphens or a hyphen followed by a number.
+    illegal_class_regex = re.compile(r"\.([0-9]|--|-[0-9])")
+    class_regex = re.compile(r"\.([a-zA-z0-9_-]+)")
+
+    path_stack: List[str] = path.split(">")
+
+    current_element = element
+
+    # Keep going until path is empty
+    while path_stack:
+        if current_element is None:
+            return None
+
+        # Take first entry from stack
+        current_entry = path_stack.pop(0)
+
+        # Illegal class name
+        if illegal_class_regex.search(current_entry) is not None:
+            # TODO raise exception to show an error message to the teacher?
+            return None
+
+        tag = tag_regex.search(current_entry)
+        id = id_regex.search(current_entry)
+        class_name = class_regex.search(current_entry)
+        index = index_regex.search(current_entry)
+
+        # Parse matches out
+        # Tag doesn't use a capture group so take match 0 instead of 1,
+        # the others need to use 1
+        if tag is not None:
+            tag = tag.group(0)
+
+        if id is not None:
+            id = id.group(1)
+
+        if class_name is not None:
+            class_name = class_name.group(1)
+
+        # Parse index out
+        if index is not None:
+            index = int(index.group(1))
+        else:
+            index = 0
+
+        # Apply filters & find a matching element
+        # None is ignored by bs4 by default which makes this a lot cleaner
+        matches = current_element.find_all(name=tag, id=id, class_=class_name)
+
+        # No matches found, or not enough
+        if not matches or len(matches) <= index:
+            return None
+
+        current_element = matches[index]
+
+    return current_element
 
 
 def compare_content(first: str, second: str) -> bool:
